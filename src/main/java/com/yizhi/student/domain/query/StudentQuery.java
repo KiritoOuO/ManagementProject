446b91efc1b836ff7ebb8c810a57ec83d50a0104
li__yaohui <960044079@qq.com>
2023-12-30 20:56:25 +0800

    Initial commit

---
+++ src/main/java/com/yizhi/student/domain/query/StudentQuery.java
@@ -0,0 +1,15 @@
+//package com.yizhi.student.domain.query;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class StudentQuery {
+//    private Integer pageNum = 1;
+//    private Integer pageSize = 2;
+//    private String name;
+//
+//}
