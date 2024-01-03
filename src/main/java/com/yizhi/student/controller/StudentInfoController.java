914653ec1d6a44bb804178eb30be0283570232d5
li__yaohui <960044079@qq.com>
2024-01-03 11:30:29 +0800

    Clear comments and code formatting

--- src/main/java/com/yizhi/student/controller/StudentInfoController.java
+++ src/main/java/com/yizhi/student/controller/StudentInfoController.java
@@ -29,123 +29,118 @@ import com.yizhi.student.service.StudentInfoService;
 /**
  * 生基础信息表
  */
- 
+
 @Controller
 @RequestMapping("/student/studentInfo")
 public class StudentInfoController {
 
 
+    @Autowired
+    private StudentInfoService studentInfoService;
 
 
-	@Autowired
-	private StudentInfoService studentInfoService;
+    /**
+     * 可分页 查询
+     */
+    @ResponseBody
+    @GetMapping("/list")
+    @RequiresPermissions("student:studentInfo:studentInfo")
+    public PageUtils list(@RequestParam Map<String, Object> params) {
+        Query query = new Query(params);
+        List<StudentInfoDO> studentInfoList = studentInfoService.list(query);
+        System.out.println(studentInfoList);
+        int total = studentInfoService.count(query);
+        PageUtils pageUtils = new PageUtils(studentInfoList, total, query.getCurrPage(), query.getPageSize());
+        return pageUtils;
+    }
 
 
-
-	/**
-	 * 可分页 查询
-	 */
-	@ResponseBody
-	@GetMapping("/list")
-	@RequiresPermissions("student:studentInfo:studentInfo")
-	public PageUtils list(@RequestParam Map<String, Object> params){
-		Query query = new Query(params);
-//		System.out.println(params);
-//		System.out.println(query);
-		List<StudentInfoDO> studentInfoList = studentInfoService.list(query);
-		System.out.println(studentInfoList);
-		int total = studentInfoService.count(query);
-//		System.out.println(total);
-		PageUtils pageUtils = new PageUtils(studentInfoList, total,query.getCurrPage(),query.getPageSize());
-//		System.out.println(pageUtils);
-		return pageUtils;
-	}
+    /**
+     * 保存
+     */
+    @Log("学生基础信息表保存")
+    @ResponseBody
+    @PostMapping("/save")
+    @RequiresPermissions("student:studentInfo:add")
+    public R save(StudentInfoDO studentInfo) {
+        if (studentInfoService.save(studentInfo) > 0) {
+            return R.ok();
+        }
+        return R.error();
+    }
 
 
-	/**
-	 * 保存
-	 */
-	@Log("学生基础信息表保存")
-	@ResponseBody
-	@PostMapping("/save")
-//	@RequiresPermissions("student:studentInfo:add")
-	public R save(StudentInfoDO studentInfo){
-		if(studentInfoService.save(studentInfo)>0){
-			return R.ok();
-		}
-		return R.error();
-	}
+    /**
+     * 修改
+     */
+    @Log("学生基础信息表修改")
+    @ResponseBody
+    @PostMapping("/update")
+    @RequiresPermissions("student:studentInfo:edit")
+    public R update(StudentInfoDO studentInfo) {
+        studentInfoService.update(studentInfo);
+        return R.ok();
 
-	/**
-	 * 修改
-	 */
-	@Log("学生基础信息表修改")
-	@ResponseBody
-	@PostMapping("/update")
-	@RequiresPermissions("student:studentInfo:edit")
-	public R update(StudentInfoDO studentInfo){
-		studentInfoService.update(studentInfo);
-		return R.ok();
+    }
 
-	}
+    /**
+     * 删除
+     */
+    @Log("学生基础信息表删除")
+    @PostMapping("/remove")
+    @ResponseBody
+    @RequiresPermissions("student:studentInfo:remove")
+    public R remove(Integer id) {
+        if (studentInfoService.remove(id) > 0) {
+            return R.ok();
+        }
+        return R.error();
+    }
 
-	/**
-	 * 删除
-	 */
-	@Log("学生基础信息表删除")
-	@PostMapping( "/remove")
-	@ResponseBody
-	@RequiresPermissions("student:studentInfo:remove")
-	public R remove( Integer id){
-		if (studentInfoService.remove(id)>0){
-			return R.ok();
-		}
-		return R.error();
-	}
-	
-	/**
-	 * 批量删除
-	 */
-	@Log("学生基础信息表批量删除")
-	@PostMapping( "/batchRemove")
-	@ResponseBody
-	@RequiresPermissions("student:studentInfo:batchRemove")
-	public R remove(@RequestParam("ids[]") Integer[] ids){
-		studentInfoService.batchRemove(ids);
-		return R.ok();
-	}
+    /**
+     * 批量删除
+     */
+    @Log("学生基础信息表批量删除")
+    @PostMapping("/batchRemove")
+    @ResponseBody
+    @RequiresPermissions("student:studentInfo:batchRemove")
+    public R remove(@RequestParam("ids[]") Integer[] ids) {
+        studentInfoService.batchRemove(ids);
+        return R.ok();
+    }
 
 
-	//前后端不分离 客户端 -> 控制器-> 定位视图
-	/**
-	 * 学生管理 点击Tab标签 forward页面
-	 */
-	@GetMapping()
-	@RequiresPermissions("student:studentInfo:studentInfo")
-	String StudentInfo(){
+    //前后端不分离 客户端 -> 控制器-> 定位视图
 
-		return "student/studentInfo/studentInfo";
-	}
+    /**
+     * 学生管理 点击Tab标签 forward页面
+     */
+    @GetMapping()
+    @RequiresPermissions("student:studentInfo:studentInfo")
+    String StudentInfo() {
 
-	/**
-	 * 更新功能 弹出View定位
-	 */
-	@GetMapping("/edit/{id}")
-	@RequiresPermissions("student:studentInfo:edit")
-	String edit(@PathVariable("id") Integer id,Model model){
-		StudentInfoDO studentInfo = studentInfoService.get(id);
-		model.addAttribute("studentInfo", studentInfo);
-		return "student/studentInfo/edit";
-	}
+        return "student/studentInfo/studentInfo";
+    }
 
-	/**
-	 * 学生管理 添加学生弹出 View
-	 */
-	@GetMapping("/add")
-	@RequiresPermissions("student:studentInfo:add")
-	String add(){
+    /**
+     * 更新功能 弹出View定位
+     */
+    @GetMapping("/edit/{id}")
+    @RequiresPermissions("student:studentInfo:edit")
+    String edit(@PathVariable("id") Integer id, Model model) {
+        StudentInfoDO studentInfo = studentInfoService.get(id);
+        model.addAttribute("studentInfo", studentInfo);
+        return "student/studentInfo/edit";
+    }
 
-	    return "student/studentInfo/add";
-	}
-	
+    /**
+     * 学生管理 添加学生弹出 View
+     */
+    @GetMapping("/add")
+    @RequiresPermissions("student:studentInfo:add")
+    String add() {
+
+        return "student/studentInfo/add";
+    }
+
 }//end class
